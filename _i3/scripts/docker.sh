79b51e1215c72c93c30d31298d57320cd034b787
Adrien Anceau <adrien.anceau@gmail.com>
2016-03-09 16:31:45 +0100

    i3: updated i3blocks docker info display

--- _i3/scripts/docker.sh
+++ _i3/scripts/docker.sh
@@ -2,7 +2,7 @@
 
 status=$(systemctl status docker | grep Active | awk '{print $2}' | sed -e "s/^active$/Started/" -e "s/^inactive$/Stopped/")
 
-echo "($(docker ps -q 2>/dev/null | wc -l))"
+echo "$(docker ps -q 2>/dev/null | wc -l) running"
 echo
 if [[ $status == Started ]]; then
   echo \#5ba7c3
