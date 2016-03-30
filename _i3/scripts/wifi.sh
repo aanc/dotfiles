80f5599e5195a317bd581cff0f35e9d433fbe53c
Adrien Anceau <adrien.anceau@gmail.com>
2016-03-30 15:41:30 +0200

    i3blocks: added wifi indicator

---
+++ _i3/scripts/wifi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+color=\#d03f3f
+for iface in $(grep -Hs "1" /sys/class/net/wl*/carrier | cut -d/ -f5)
+do
+  essid=$(iwgetid | grep "^$iface" | sed -r -e 's/.*ESSID:"(.*)"/\1/g')
+  [[ -n $essid ]] && color=\#5ba7c3 || essid="disconnected"
+  echo -n "$iface:$essid"
+done
+
+echo
+echo
+echo $color
