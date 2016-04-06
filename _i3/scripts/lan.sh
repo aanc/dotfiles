901f2871fdb578f4ff96e9ee6bb4ceac14306cf1
Adrien Anceau <adrien.anceau@gmail.com>
2016-04-06 15:16:49 +0200

    i3blocks: reduced lan & wifi blocks size

--- _i3/scripts/lan.sh
+++ _i3/scripts/lan.sh
@@ -4,7 +4,7 @@ color=\#d03f3f
 for iface in $(grep -Hs "1" /sys/class/net/eno*/carrier /sys/class/net/eth*/carrier | cut -d/ -f5)
 do
   speed=$(ethtool eno1 2>/dev/null | grep Speed | tr -d " " | cut -d: -f2)
-  echo -n "$iface:$speed"
+  echo -n "$speed"
   color=\#5ba7c3
 done
 
--- _i3/scripts/wifi.sh
+++ _i3/scripts/wifi.sh
@@ -5,7 +5,7 @@ for iface in $(grep -Hs "1" /sys/class/net/wl*/carrier | cut -d/ -f5)
 do
   essid=$(iwgetid | grep "^$iface" | sed -r -e 's/.*ESSID:"(.*)"/\1/g')
   [[ -n $essid ]] && color=\#5ba7c3 || essid="disconnected"
-  echo -n "$iface:$essid"
+  echo -n "$essid"
 done
 
 echo
