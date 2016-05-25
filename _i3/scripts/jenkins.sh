40abdee50e37818eae3ca0603a0a60ce8316354a
Adrien Anceau <adrien.anceau@gmail.com>
2016-05-25 17:13:11 +0200

    i3 bar: updated jenkins status block

--- _i3/scripts/jenkins.sh
+++ _i3/scripts/jenkins.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 wall=$(curl -s http://forge/ic/view/_FDXT_DSL/view/Exploitation/view/Wall/ | sed -e "s|</div>|</div>\n|g" | grep 'class="job' | sed -r -e 's|^.* class="job ([a-z]*) .*$|\1|')
-[[ -z $wall ]] && echo "unreachable" && echo && echo \#d03f3f
+[[ -z $wall ]] && echo "Jenkins" && echo && echo \#d03f3f
 
 total=$(wc -l <<< $wall)
 failed=$(egrep -o "failing|claimed" <<< $wall | wc -l)
 
 if [[ $failed -gt 0 ]]; then
-	echo "Jenkins: $failed KO"
+	echo "Jenkins:$failed KO"
 	echo
 	echo \#cc5b5b
 else
