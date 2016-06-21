279c97686162f71cc442d157a77322acaa530e04
Adrien Anceau <adrien.anceau@gmail.com>
2016-06-21 17:14:39 +0200

    i3bar: wifi essid name in white

--- _i3/scripts/wifi.sh
+++ _i3/scripts/wifi.sh
@@ -4,7 +4,7 @@ color=\#d03f3f
 for iface in $(grep -Hs "1" /sys/class/net/wl*/carrier | cut -d/ -f5)
 do
   essid=$(iwgetid | grep "^$iface" | sed -r -e 's/.*ESSID:"(.*)"/\1/g')
-  [[ -n $essid ]] && color=\#5ba7c3 || essid="disconnected"
+  [[ -n $essid ]] && color=\#ffffff || essid="disconnected"
   echo -n "$essid"
 done
 
