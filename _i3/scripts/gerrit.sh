cd0b7b773fed496b08ac8185a908a6629546c642
Adrien Anceau <adrien.anceau@gmail.com>
2016-06-22 11:17:32 +0200

    i3bar: jenkins block click action improved
    - now support claimed jobs
    - displays if job is claimed
    - fix issue where block disappeared when a rofi entry was selected
    +
    - mutualization of colors with gerrit block

--- _i3/scripts/gerrit.sh
+++ _i3/scripts/gerrit.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+source $SCRIPT_DIR/_common.sh
+
 count=$(curl -s 'http://git:8080/gerrit/changes/' | egrep -c '"project": "forge/forge-config-mgt"|"project": "forge/forge-jobs"')
 if [[ $count -gt 0 ]]; then
 	echo "Gerrit: $count waiting"
 	echo
-	echo \#cc5b5b
+	echo $COLOR_RED_HEX
 else
 	echo "Gerrit:OK"
 	echo
-	echo \#6BC289
+	echo $COLOR_GREEN_HEX
 fi
--- _i3/scripts/jenkins.sh
+++ _i3/scripts/jenkins.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
+source $SCRIPT_DIR/_common.sh
 source $SCRIPT_DIR/_firefox.sh
 
 wallURL="http://forge/ic/view/_FDXT_DSL/view/Exploitation/view/Wall/"
 jobsURL="http://forge/ic/job/"
 
-if [[ -z $BLOCK_BUTTON ]]; then
-	wall=$(curl -s $wallURL | sed -e "s|</div>|</div>\n|g" | grep 'class="job' | sed -r -e 's|^.* class="job ([a-z]*) .*$|\1|')
-	[[ -z $wall ]] && echo "Jenkins" && echo && echo \#d03f3f
+wall=$(curl -s $wallURL | sed -e "s|</div>|</div>\n|g" | grep 'class="job' | sed -r -e 's|^.* tooltip="(.*)" .* class="job ([a-z]*) .*$|\1:\2|')
 
-	total=$(wc -l <<< $wall)
-	failed=$(egrep -o "failing|claimed" <<< $wall | wc -l)
+if [[ -n $BLOCK_BUTTON ]]; then
+	jobName=$(egrep "failing|claimed" <<< "$wall" | sed -r -e 's/:(.*)$/ (\1)/' | rofi -dmenu -no-fullscreen -font "DejaVu Sans Mono 10" -p "Failing jobs:" -padding 2 -location 3 -yoffset 24 -lines 5 | cut -d' ' -f1)
+	[[ -n $jobName ]] && firefox::open_in_new_tab ${jobsURL}/${jobName} &>/dev/null
+fi
+	[[ -z $wall ]] && echo "Jenkins" && echo && echo \#d03f3f && exit 0
 
-	if [[ $failed -gt 0 ]]; then
-		echo "Jenkins:$failed KO"
-		echo
-		echo \#cc5b5b
-	else
-		echo 'Jenkins:OK'
-		echo
-		echo \#6BC289
-	fi
+total=$(wc -l <<< $wall)
+failed=$(egrep -o "failing|claimed" <<< "$wall" | wc -l)
+
+if [[ $failed -gt 0 ]]; then
+	echo "Jenkins:$failed KO"
+	echo
+	echo $COLOR_RED_HEX
 else
-	jobName=$(curl -s $wallURL | sed -e "s|</div>|</div>\n|g" | grep 'class="job' | sed -r -e 's|^.* tooltip="(.*)" .* class="job ([a-z]*) .*$|\2 \1|' | grep ^failing | cut -d" " -f2 | rofi -dmenu -no-fullscreen -font "DejaVu Sans Mono 10" -p "Failing jobs:" -padding 2 -location 3 -yoffset 24 -lines 5)
-	[[ -n $jobName ]] && firefox::open_in_new_tab ${jobsURL}/${jobName}
+	echo 'Jenkins:OK'
+	echo
+	echo $COLOR_GREEN_HEX
 fi
